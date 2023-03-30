b991bd798d789590f56124c91f842a8904873d57
kaushik <kyadunath@gmail.com>
2023-03-30 20:09:57 +0800

    problem 10 done

--- Solutions/p010.py
+++ Solutions/p010.py
@@ -1,7 +1,14 @@
 # Summation of Primes
 
 def solution():
-
+    primes = [2]
+    for i in range(3, 2000000, 2):
+        for j in primes:
+            if i % j == 0:
+                break
+        else:
+            primes.append(i)
+    return sum(primes)
     
 
 if __name__ == "__main__":
